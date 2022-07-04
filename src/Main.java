a986971f590002231bf528f9a791c851f4c85cf1
NikitaBez <bezmenov.nikita@gmail.com>
2022-07-04 23:09:17 +0900

    programm added

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 20000;
+        int rubmile = 20;
+
+
+        int mile = price / rubmile;
+        System.out.println(" Вы получили " + mile + " бонусных миль ");
+
+
+    }
+}
