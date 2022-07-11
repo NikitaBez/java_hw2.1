b067fda59e8591b249ca3df7832edba5e205e4ad
NikitaBez <bezmenov.nikita@gmail.com>
2022-07-11 22:49:11 +0900

    Added BonusMilesService class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 20000;
-        int rubmile = 20;
-
-
-        int mile = price / rubmile;
-        System.out.println(" Вы получили " + mile + " бонусных миль ");
-
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 100_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
